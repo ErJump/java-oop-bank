d4f3cd2e907b438cbd4bef4c00988fe184e53267
ErJump <fedelijunior@gmail.com>
2022-11-18 09:13:51 +0100

    feat: :sparkles: Adds class Conto

---
+++ src/jana60/bank/Conto.java
@@ -0,0 +1,36 @@
+package jana60.bank;
+
+public class Conto {
+	private int numeroConto;
+	private String nomeProprietario;
+	private int saldo;
+	
+	Conto(int numeroConto, String nomeProprietario ) {
+		this.numeroConto = numeroConto;
+		this.nomeProprietario = nomeProprietario;
+		saldo = 0;
+	}
+	
+	public int getNumeroConto() {
+		return numeroConto;
+	}
+	
+	public int getSaldo() {
+		return saldo;
+	}
+	
+	public String getNomeProprietario() {
+		return nomeProprietario;
+	}
+	
+	public void setNomeProprietario(String nomeProprietario) {
+		this.nomeProprietario = nomeProprietario;
+	}
+	
+	@Override
+	public String toString() {
+		return "nome proprietario: " + getNomeProprietario() 
+				+ "\nnumero conto: " + getNumeroConto() 
+				+ "\nsaldo: " + getSaldo() + "$";
+	}
+}
