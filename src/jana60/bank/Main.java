4d7754870dbfd42fddc1c7de2e38937236378d23
ErJump <fedelijunior@gmail.com>
2022-11-18 09:33:03 +0100

    feat: :sparkles: Adds one more object to test features in main class

--- src/jana60/bank/Main.java
+++ src/jana60/bank/Main.java
@@ -16,5 +16,15 @@ public class Main {
 		c1.getMoney(20);
 		System.out.println(c1.getSaldo() + "$");
 		c1.getMoney(10);
+
+		//aggiungo un conto e testo le funzionalità
+		Conto c2 = new Conto(5678, "Giovanni Rossi");
+		c2.addMoney(100);
+		System.out.println(c2);
+		System.out.println("--------------");
+		c2.getMoney(50);
+		System.out.println("saldo c2: " + c1.getSaldo() + "$");
+		System.out.println("--------------");
+		c2.getMoney(100);		
 	}
 }
